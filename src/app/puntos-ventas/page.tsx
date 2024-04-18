c92801048e13cbfd30b219a7a8ce929ae1924d9b
Joe Corrales <corralesjoe07@gmail.com>
2024-04-17 19:00:17 -0600

    maps

--- src/app/puntos-ventas/page.tsx
+++ src/app/puntos-ventas/page.tsx
@@ -1,5 +1,9 @@
 export default function PuntosVentas() {
     return (
-        <h1>Pagina de puntos de venta</h1>
+        <section
+                 className="w-full flex flex-col lg:px-[64px] lg:py-[112px] px-[20px] py-[64px] gap-[48px] lg:gap-[80px]">
+            <iframe src="https://www.google.com/maps/d/embed?mid=1LFXd0xuycKHMAxOy25obKVFNxfucVZY&ehbc=2E312F"
+                    width="100%" height="512" className=""></iframe>
+        </section>
     );
 }
